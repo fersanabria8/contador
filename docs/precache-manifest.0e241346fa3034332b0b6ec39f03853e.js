f6ebe7dc5b4804108162a0f1c8614f4e7cc55fb4
fersanabria8 <34108851+fersanabria8@users.noreply.github.com>
2022-04-15 03:07:06 -0300

    Add files via upload

---
+++ docs/precache-manifest.0e241346fa3034332b0b6ec39f03853e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e0aeeddd51886571fbd",
+    "url": "/static/css/main.fbe2f50d.chunk.css"
+  },
+  {
+    "revision": "5e0aeeddd51886571fbd",
+    "url": "/static/js/main.5e0aeedd.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "b36f8949cb984f4794ca",
+    "url": "/static/js/2.b36f8949.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "36aa7cad93354cd1ef7180c33ca44da8",
+    "url": "/index.html"
+  }
+];
